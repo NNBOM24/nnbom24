1352834be5620f7a92bffd131ba809a9ce80941e
NNBOM24 <2652620138@qq.com>
2024-06-18 17:51:10 +0800

    update

--- component_analysis/python_antlr/src/main/java/process/MyPythonListener.java
+++ component_analysis/python_antlr/src/main/java/process/MyPythonListener.java
@@ -11,11 +11,9 @@ import antlr4py.Python3ParserBaseListener;
 public class MyPythonListener extends Python3ParserBaseListener {
     @Override
     public void enterClassdef(Python3Parser.ClassdefContext ctx) {
-        // 进入类定义时的处理逻辑
         String className = ctx.getText();
         String superClass = null;
 
-        // 检查是否有基类
         if (ctx.arglist() != null && ctx.arglist().argument().size() > 0) {
             superClass = ((RuleContext) ctx.arglist().argument(0).test()).getText();
         }
